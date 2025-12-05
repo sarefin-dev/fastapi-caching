8bf567250f99009cd2d0b74d6a0cdfbb2c65de5a
Sadot Arefin <sadot.arefin@gmail.com>
2025-12-05 14:06:09 +0600

    fix: factorial was called instead of fabonacci - fixed

--- with-cachetool/app/routes/basic_computation.py
+++ with-cachetool/app/routes/basic_computation.py
@@ -32,6 +32,28 @@ def _compute_sum(x: list[int]) -> int:
     return sum(x)
 
 
+# NEW FEATURE: Recursive fibonacci computation with default caching
+@cached(cache)
+def _compute_fibonacci(n) -> int:
+    """
+    Docstring for _compute_fibonacci
+
+    :param n: the number for which the fabonacci series will be calculated
+    :return: fibonacci series is a series where a number is the sum of two consecutive previous number.
+    :rtype: int
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        # This print statement helps verify cache hits/misses
+        print(
+            f"Computing fibonacci for {n} - it should not be called within 1 min if number <= {n} is given"
+        )
+        return _compute_fibonacci(n - 2) + _compute_factorial(n - 1)
+
+
 # NEW FEATURE: Recursive factorial computation with default caching
 # The default key generator for @cached uses the function arguments (n) directly.
 @cached(cache)
@@ -63,3 +85,10 @@ async def get_sum(q: list[int] = Query()):
 async def get_factorial(n: int) -> int:
     """Endpoint for computing the factorial of an integer 'n'."""
     return _compute_factorial(n)
+
+
+@router.get("/fibonacci/{n}")
+async def get_fibonacci(n: int) -> int:
+    """Endpoint for computing the factorial of an integer 'n'."""
+    return _compute_fibonacci(n)
+
